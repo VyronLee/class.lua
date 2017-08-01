62cc6432053db1d16789dbf2fef49cd020748991
VyronLee <lwz_jz@hotmail.com>
2017-08-01 20:08:00 +0800

    Fix error when assigning new key to an instance.

--- spec/class_static_member_accessibility_spec.lua
+++ spec/class_static_member_accessibility_spec.lua
@@ -36,5 +36,25 @@ describe("A static member", function()
             print(instance.width)
         end)
     end)
+end)
 
+describe("An instance", function()
+    it("cannot assign new value with key 'static'", function()
+        local classA = class("ClassA")
+        local instance = classA()
+
+        assert.has_error(function()
+            instance.static = "new assign value"
+        end)
+    end)
+
+    it("cannot call it's static member/methods directly", function()
+        local classA = class("ClassA")
+        classA.static.s_value = "s_value"
+
+        local instance = classA()
+        local s_value
+        assert.has.no_error(function() s_value = classA.s_value end)
+        assert.has_error(function() s_value = instance.s_value end)
+    end)
 end)
