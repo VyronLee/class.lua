b7b9d5f2f500f37a22864480c01e7cf415fb2d55
VyronLee <lwz_jz@hotmail.com>
2017-09-02 23:39:49 +0800

    Modify: Add the missing unit tests.

--- spec/classA-impl.lua
+++ spec/classA-impl.lua
@@ -1,5 +1,6 @@
 local NUMBER_6 = 6
 local NUMBER_7 = 7
+local pow = math.pow
 
 function initialize(self)
     self.number6 = NUMBER_6
@@ -7,4 +8,8 @@ end
 
 function number_7()
     return NUMBER_7
+end
+
+function pow_2_of_7(self)
+    return pow(self.number_7(), 2)
 end
